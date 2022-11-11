a3e3ab34f0820751acbac8f5f7e924654cb2c698
onowdev <onowdev@gmail.com>
2022-11-11 21:15:19 +0700

    add gitar

---
+++ src/com/onowdev/javafundamental/basic/Main.java
@@ -0,0 +1,11 @@
+package com.onowdev.javafundamental.basic;
+
+import com.onowdev.javafundamental.basic.musik.Gitar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        Gitar.Bunyi();
+    }
+}
---
+++ src/com/onowdev/javafundamental/basic/musik/Gitar.java
@@ -0,0 +1,7 @@
+package com.onowdev.javafundamental.basic.musik;
+
+public class Gitar {
+    public static void Bunyi() {
+        System.out.println("Jrenggg");
+    }
+}
