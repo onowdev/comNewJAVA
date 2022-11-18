3de03200a728acedf2955d3697d9172dbcf14b33
onowdev <onowdev@gmail.com>
2022-11-18 21:40:20 +0700

    rif works

--- src/com/onowdev/javafundamental/basic/kendaraan/Mobil.java
+++ src/com/onowdev/javafundamental/basic/kendaraan/Mobil.java
@@ -4,4 +4,11 @@ public class Mobil {
     public static void jumlahBan(){
         System.out.println("Ban Mobil berjumlah 4");
     }
+
+    public static void drive() {
+        String s = "brum";
+        if (s != null) {
+            System.out.println("s = " + s);
+        }
+    }
 }
