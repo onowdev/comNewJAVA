882c15e244c7ccb07c9cb15bc3d19e16c2cd6853
onowdev <onowdev@gmail.com>
2022-11-21 19:54:56 +0700

    looping Array

---
+++ src/com/onowdev/javafundamental/basic/array/LoopingArray.java
@@ -0,0 +1,13 @@
+package com.onowdev.javafundamental.basic.array;
+
+public class LoopingArray {
+    public static void main(String[] args) {
+        int [] arrInt = new int [1000000];
+
+        // inisiasi dan menampilkan arrInt
+        for (int x = 0; x < arrInt.length);
+            arrInt[x] = x+1;
+        System.out.println(arrInt[x]);
+
+    }
+}
