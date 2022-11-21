9b3cabe30ec1ac811c799692f9ae8bc86d110ce5
onowdev <onowdev@gmail.com>
2022-11-21 15:20:38 +0700

    Inisiasi Array versi 2

--- src/com/onowdev/javafundamental/basic/array/InisiasiArray.java
+++ src/com/onowdev/javafundamental/basic/array/InisiasiArray.java
@@ -10,5 +10,27 @@ public class InisiasiArray {
         System.out.println(arrInt[3]);
         System.out.println(arrInt[4]);
         System.out.println(arrInt[5]);
+
+    // Cara Lain inisialisasi Array
+
+            int [] arrInt2 = new int[7]; // menentukan variabel dan Panjang Array
+            arrInt2[0] = 1;
+            arrInt2[1] = 2;
+            arrInt2[2] = 3;
+            arrInt2[3] = 4;
+            arrInt2[4] = 5;
+            arrInt2[5] = 6;
+            arrInt2[6] = 7;
+            //arrInt2[7] = 8;
+
+            System.out.println(arrInt2[0]);
+            System.out.println(arrInt2[1]);
+            System.out.println(arrInt2[2]);
+            System.out.println(arrInt2[3]);
+            System.out.println(arrInt2[4]);
+            System.out.println(arrInt2[5]);
+            System.out.println(arrInt2[6]);
+            //System.out.println(arrInt2[7]);
+
     }
 }
