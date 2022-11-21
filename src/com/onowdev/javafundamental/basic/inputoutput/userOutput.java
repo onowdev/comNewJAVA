2d6a30c0fa02c5df526b928943d0678562120252
onowdev <onowdev@gmail.com>
2022-11-21 11:46:53 +0700

    User Output

---
+++ src/com/onowdev/javafundamental/basic/inputoutput/userOutput.java
@@ -0,0 +1,11 @@
+package com.onowdev.javafundamental.basic.inputoutput;
+
+public class userOutput {
+    public static void main(String[] args) {
+        System.out.println("dicetak menggukanan fungsi println");
+        System.out.println("Ini juga di cetak meggunakan fungsi println");
+        System.out.print("Ini dicetak menggunakan Fungsi print saja");
+        System.out.println("Ini Juga dicetak menggunakan fungsi print saja tanpa ln");
+
+    }
+}
