2f90ecc18b351d5816a52f0e29ee4089e77c0391
onowdev <onowdev@gmail.com>
2022-11-22 22:22:20 +0700

    java collection

---
+++ src/com/onowdev/javafundamental/basic/collections/Main.java
@@ -0,0 +1,10 @@
+package com.onowdev.javafundamental.basic.collections;
+
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection collection;
+
+    }
+}
