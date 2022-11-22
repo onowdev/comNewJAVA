20ecd60d366dfd71acc437e23e76741f9976300f
onowdev <onowdev@gmail.com>
2022-11-22 10:30:59 +0700

    ifElse sederhana

---
+++ src/com/onowdev/javafundamental/basic/percabangan/IfThen.java
@@ -0,0 +1,11 @@
+package com.onowdev.javafundamental.basic.percabangan;
+
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+
+        if (isOn) {
+            System.out.println("Menyalakan Lampu");
+        }
+    }
+}
