5122f4a5c292e1e94c2a75018672094055f5e180
onowdev <onowdev@gmail.com>
2022-11-22 11:52:17 +0700

    while

---
+++ src/com/onowdev/javafundamental/basic/perulangan/While.java
@@ -0,0 +1,12 @@
+package com.onowdev.javafundamental.basic.perulangan;
+
+public class While {
+    public static void main(String[] args) {
+        int value = 1;
+        while (value <= 10) {
+            System.out.println("Angka : " + value);
+            value++;
+            System.out.println("\n");
+        }
+    }
+}
