83bbb40c3c0a30c65bf0c784b9b27fbb82dcda4a
onowdev <onowdev@gmail.com>
2022-11-22 11:53:48 +0700

    DO_while

---
+++ src/com/onowdev/javafundamental/basic/perulangan/DoWhile.java
@@ -0,0 +1,11 @@
+package com.onowdev.javafundamental.basic.perulangan;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int value = 1;
+        do{
+            System.out.println("Angka : " +  value);
+            value++;
+        } while (value <= 10);
+    }
+}
