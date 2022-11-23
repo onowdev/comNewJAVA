df8b2f358e0be4d26582dc0797ab88ef0ea1fd09
onowdev <onowdev@gmail.com>
2022-11-23 15:16:23 +0700

    pembuatan fungsi

---
+++ src/com/onowdev/javafundamental/basic/function/CallFunction.java
@@ -0,0 +1,12 @@
+package com.onowdev.javafundamental.basic.function;
+
+public class CallFunction {
+    public static void main(String[] args) {
+        // memanggil fungsi
+        cobaFungsi();
+    }
+
+    public static void cobaFungsi(){
+        System.out.println("Ini Merupakan Bagian dari Fungsi");
+    }
+}
