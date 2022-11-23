3bb47c95b459eabd9bdd595eb2ea6fbd59971403
onowdev <onowdev@gmail.com>
2022-11-23 12:20:35 +0700

    java collection Map

---
+++ src/com/onowdev/javafundamental/basic/array/MapPlanet.java
@@ -0,0 +1,32 @@
+package com.onowdev.javafundamental.basic.array;
+
+import com.onowdev.javafundamental.basic.collections.Planet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPlanet {
+    public static void main(String[] args) {
+        Map<String, Planet> planets = new HashMap<>();
+        planets.put("key-1", new Planet("Mercury", 0.06)); // method put() untuk menambahkan objek ke Map
+        planets.put("key-2", new Planet("Venus", 0.82));
+        planets.put("key-3", new Planet("Earth", 1.00));
+        planets.put("key-4", new Planet("Mars", 0.11));
+        planets.put("key", new Planet("Mars-X", 0.11)); // menambahkan ke Map dengan key yang sama
+
+        // Method size() untuk mendapatkan ukuran Set
+        System.out.println("Map planets awal : (size = "+ planets.size() +  ")");
+        for (String key : planets.keySet()) { // looping key dari Map
+            // method get() untuk melihat isi Map berdasarkan key
+            System.out.println("\t " + key + " : " +planets.get(key));
+        }
+
+        planets.remove("key-2"); // Method remove() untuk mengeluarkan objek dari Map
+
+        System.out.println("Map planets akhir : (size = "+ planets.size() +")");
+        for (Planet planet : planets.values()){ // looping value dari Map
+            System.out.println("\t " + planet);
+        }
+    }
+
+}
---
+++ src/com/onowdev/javafundamental/basic/collections/Planet.java
@@ -0,0 +1,16 @@
+package com.onowdev.javafundamental.basic.collections;
+
+public class Planet {
+    private String name;
+    private double mass;
+
+    public Planet(String name, double mass){
+        this.name = name;
+        this.mass = mass;
+    }
+
+    @Override
+    public String toString(){
+        return "Planet" + name + ", mass:" +mass;
+    }
+}
