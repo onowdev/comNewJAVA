897848f94422fd447a844aa387fc7dc1eb66385e
onowdev <onowdev@gmail.com>
2022-11-24 11:23:53 +0700

    pembuatan fungsi return and compilenya

--- src/com/onowdev/javafundamental/basic/function/ReturnFunction.java
+++ src/com/onowdev/javafundamental/basic/function/ReturnFunction.java
@@ -1,5 +1,20 @@
 package com.onowdev.javafundamental.basic.function;
 
 public class ReturnFunction {
+    public static void main(String[] args) {
+        double p = 7;
+        double l = 6.5;
+
+        double hasil = hitungLuas(p, l); // memanggil fungsi
+
+        System.out.println("Hasilnya adalah = " + hasil);
+    }
+
+    // Fungsi dengan Nilai Balik
+
+    public static double hitungLuas(double panjang, double lebar){
+        double luas = panjang * lebar;
+        return luas;
+    }
 
 }
