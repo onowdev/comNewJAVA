d2175386ae05b6fddc7520f9eef0fc784a315e38
onowdev <onowdev@gmail.com>
2022-11-27 22:23:25 +0700

    Method Overloading

---
+++ src/com/onowdev/javafundamental/basic/propertimetode/Hewan.java
@@ -0,0 +1,17 @@
+package com.onowdev.javafundamental.basic.propertimetode;
+
+public class Hewan {
+
+    //properti atau fields
+    //inisiasi dengan nilai inisial
+    double tinggi = 30;
+    double berat = 3;
+
+    // Inisiasi melalui Konstruktor
+    int umur;
+
+    // Konstruktor dengan parameter
+    Hewan(int umurParam) {
+        umur = umurParam;
+    }
+}
---
+++ src/com/onowdev/javafundamental/basic/ujian/HelloWord.java
@@ -0,0 +1,10 @@
+package com.onowdev.javafundamental.basic.ujian;
+
+public class HelloWord {
+    public static void main(String[] args) {
+
+        int a = (int) 4.17;
+
+        System.out.println(a);
+    }
+}
