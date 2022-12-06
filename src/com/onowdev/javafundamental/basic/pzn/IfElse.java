4082b7fd49243e834b9b147fb803c39c73be8d30
onowdev <onowdev@gmail.com>
2022-12-06 20:52:30 +0700

    new Jva

---
+++ src/com/onowdev/javafundamental/basic/pzn/IfElse.java
@@ -0,0 +1,13 @@
+package com.onowdev.javafundamental.basic.pzn;
+
+public class IfElse {
+    public static void main(String[] args) {
+        var nilai = 90;
+        var absen = 90;
+
+        if (nilai >=70 && absen >= 80){
+            System.out.println("Selamat Anda Lulus");
+        }
+
+    }
+}
---
+++ src/com/onowdev/javafundamental/basic/pzn/IfElseHcrrnk.java
@@ -0,0 +1,19 @@
+package com.onowdev.javafundamental.basic.pzn;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class IfElseHcrrnk {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
